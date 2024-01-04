c0f53ffc4894a6a0cb85ab54db9332a76527ff7c
Mykhailo Koshmak <peschernuy@gmail.com>
2024-01-04 16:30:44 +0200

    03.js

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -6,3 +6,15 @@ if (number1 % 2 === 0) {
 } else {
   console.log(`${number1} Это нечетное число`);
 }
+
+
+let number = 1
+
+console.log(number++)
+console.log(++number)
+console.log(number--)
+console.log(--number)
+
+
+
+
