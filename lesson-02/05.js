69b3ead39191323256e8712c876fb90167614a93
Mykhailo Koshmak <peschernuy@gmail.com>
2024-01-08 17:59:05 +0200

    02-05

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -1,1 +1,12 @@
-// code
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+
+let anotherObj = Object.assign({}, passport);
+anotherObj.name = 'Ivan'
+
+
+console.log(passport)
+console.log(anotherObj)
