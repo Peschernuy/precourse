6b5229cde087b54a400d06eac2235848d72006f2
Mykhailo Koshmak <peschernuy@gmail.com>
2024-01-08 18:14:37 +0200

    02-07

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -1,1 +1,20 @@
-// code
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+// Создаем глубокую копию объекта passportMarried и добавляем ключ married
+let passportMarried2 = {
+    ...passportMarried,
+    married: true,
+    address: {
+        ...passportMarried.address
+    }
+};
+
+console.log(passportMarried);
+console.log(passportMarried2);
