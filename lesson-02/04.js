3d7a289da6ac868bcd964b6e8d96647832ec0d28
Mykhailo Koshmak <peschernuy@gmail.com>
2024-01-08 17:50:08 +0200

    02-04

--- lesson-02/04.js
+++ lesson-02/04.js
@@ -1,1 +1,10 @@
-// code
+const giveMeRightCondition = (value1, value2, maximum) => {
+    if ( value1 === value2 && value1 <=  maximum) {
+        console.log('Success!');
+    } else {
+        console.log('Something wrong!');
+    }
+}
+
+giveMeRightCondition(201, 100, 200)
+
