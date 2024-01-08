6016e102c0443fdfd3f50d8edd79ed7cfbcd6630
Mykhailo Koshmak <peschernuy@gmail.com>
2024-01-08 18:08:09 +0200

    02-06

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -1,1 +1,15 @@
-// code
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+
+let newPassport = JSON.parse(JSON.stringify(passportWithAddress));
+newPassport.address.city = 'Bobryisk';
+
+console.log(passportWithAddress)
+console.log(newPassport)
